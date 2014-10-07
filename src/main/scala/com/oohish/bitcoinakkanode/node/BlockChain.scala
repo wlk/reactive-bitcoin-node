3ee9b077fb89a266228546528b3d4a1027ccbd15
yzernik <yzernik@gmail.com>
2014-10-06 21:49:19 -0700

    added hash function to utils

--- src/main/scala/com/oohish/bitcoinakkanode/node/BlockChain.scala
+++ src/main/scala/com/oohish/bitcoinakkanode/node/BlockChain.scala
@@ -6,23 +6,38 @@ import akka.actor.ActorLogging
 import akka.actor.Props
 import com.oohish.bitcoinscodec.messages.Block
 import com.oohish.bitcoinscodec.structures.Hash
+import com.oohish.bitcoinakkanode.util.Util
 
 object BlockChain {
   def props(networkParams: NetworkParameters) =
     Props(classOf[BlockChain], networkParams)
 
   case class StoredBlock(b: Block, height: Long)
+  case class GetHeight()
+  case class GetHeightResponse(height: Long)
+
 }
 
-class BlockChain(networkParams: NetworkParameters) extends Actor with ActorLogging {
+trait BlockChain extends Actor with ActorLogging {
   import com.oohish.bitcoinakkanode.node.BlockChain._
+  import scodec.bits.BitVector
+  import com.oohish.bitcoinscodec.structures.BlockHeader
 
-  val genesis = StoredBlock(networkParams.genesisBlock, 1)
+  val genesis: Block
+  val g = StoredBlock(genesis, 1)
   var blocks: Map[Hash, StoredBlock] = Map.empty
-  var chainHead = genesis
+  var chainHead = StoredBlock(genesis, 1)
 
   def receive = {
-    case _ =>
+    case GetHeight() =>
+      sender ! GetHeightResponse(chainHead.height)
   }
 
+  def isValidBlock(b: Block): Boolean
+
+  def blockHash(b: Block): Hash = {
+    val bytes = BlockHeader.codec.encode(b.block_header)
+      .getOrElse(BitVector.empty).toByteArray
+    Util.blockHash(bytes)
+  }
 }
--- src/main/scala/com/oohish/bitcoinakkanode/util/Util.scala
+++ src/main/scala/com/oohish/bitcoinakkanode/util/Util.scala
@@ -3,6 +3,9 @@ package com.oohish.bitcoinakkanode.util
 import scala.util.Random
 import scodec.bits.ByteVector
 import java.net.InetAddress
+import com.oohish.bitcoinscodec.structures.Hash
+import com.oohish.bitcoinscodec.messages.Block
+import java.security.MessageDigest
 
 object Util {
 
@@ -10,6 +13,13 @@ object Util {
     val bytes: Array[Byte] = Array.fill(8)(0)
     Random.nextBytes(bytes)
     BigInt(0.toByte +: bytes)
+  }
+
+  def blockHash(bytes: Array[Byte]): Hash = {
+    val messageDigest = MessageDigest.getInstance("SHA-256")
+    val hash1 = messageDigest.digest(bytes)
+    val hash2 = messageDigest.digest(hash1)
+    Hash(ByteVector(hash2))
   }
 
 }
