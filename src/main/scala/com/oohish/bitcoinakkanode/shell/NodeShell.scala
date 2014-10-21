a548f9031e48213bba405faa554b2e5cdba44b95
yzernik <yzernik@gmail.com>
2014-10-21 01:42:53 -0700

    added testnet3 option to command line

--- src/main/scala/com/oohish/bitcoinakkanode/shell/NodeShell.scala
+++ src/main/scala/com/oohish/bitcoinakkanode/shell/NodeShell.scala
@@ -3,20 +3,27 @@ package com.oohish.bitcoinakkanode.shell
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 import scala.io.StdIn
+import scala.language.postfixOps
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
-import scala.language.postfixOps
-import com.oohish.bitcoinakkanode.node.Node._
+
+import com.oohish.bitcoinakkanode.node.Node.APICommand
+import com.oohish.bitcoinakkanode.node.Node.GetBestBlockHash
+import com.oohish.bitcoinakkanode.node.Node.GetBlockCount
+import com.oohish.bitcoinakkanode.node.Node.GetBlockHash
+import com.oohish.bitcoinakkanode.node.Node.GetConnectionCount
+import com.oohish.bitcoinakkanode.node.Node.GetPeerInfo
 import com.oohish.bitcoinakkanode.node.SPVNode
 import com.oohish.bitcoinakkanode.wire.MainNetParams
+import com.oohish.bitcoinakkanode.wire.NetworkParameters
+import com.oohish.bitcoinakkanode.wire.TestNet3Params
+
+import akka.actor.ActorRef
 import akka.actor.ActorSystem
+import akka.actor.Props
 import akka.pattern.ask
 import akka.util.Timeout
-import com.oohish.bitcoinakkanode.wire.NetworkParameters
-import akka.actor.Props
-import java.io.File
-import akka.actor.ActorRef
 
 object NodeShell {
 
@@ -30,8 +37,8 @@ object NodeShell {
     opt[String]('n', "network name") action {
       case ("main", c) =>
         c.copy(network = MainNetParams)
-      //      case ("test", c) =>
-      //        c.copy(network = TestNet3Params)
+      case ("test", c) =>
+        c.copy(network = TestNet3Params)
     } text ("network is a String property")
   }
 
@@ -47,7 +54,6 @@ object NodeShell {
   def main(args: Array[String]) = {
 
     parser.parse(args, Config()) map { config =>
-      println(config)
 
       val prompt = "bitcoin-akka-node> "
       println("Welcome to bitcoin-akka-node.")
--- src/main/scala/com/oohish/bitcoinakkanode/wire/NetworkParameters.scala
+++ src/main/scala/com/oohish/bitcoinakkanode/wire/NetworkParameters.scala
@@ -1,12 +1,12 @@
 package com.oohish.bitcoinakkanode.wire
 
 import scala.math.BigInt.int2bigInt
-import com.oohish.bitcoinscodec.messages._
+
+import com.oohish.bitcoinscodec.messages.Block
 import com.oohish.bitcoinscodec.structures.BlockHeader
 import com.oohish.bitcoinscodec.structures.Hash
-import scodec.bits.ByteVector
 
-import scodec.bits._
+import scodec.bits.HexStringSyntax
 
 /*
  * Taken from bitcoinj's NetworkParameters
@@ -27,50 +27,6 @@ trait NetworkParameters {
   val TARGET_SPACING: Int = 10 * 60; // 10 minutes per block.
   val INTERVAL: Int = TARGET_TIMESPAN / TARGET_SPACING;
 }
-
-/*
-object MainNetParams extends NetworkParameters {
-
-  def addressHeader: Byte = 0
-  def genesisBlock: Block =
-    Block(
-      1, //version
-      "0000000000000000000000000000000000000000000000000000000000000000", //prev block
-      "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", //merkle root
-      1231006505, //timestamp
-      486604799, //bits
-      2083236893, //nonce
-      List(
-        Tx(
-          1, //version
-          List(
-            TxIn(
-              OutPoint(
-                "0000000000000000000000000000000000000000000000000000000000000000", //prev block
-                4294967295L //n
-                ),
-              "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73", // txins
-              Int.MaxValue.toLong * 2)),
-          List(
-            TxOut(
-              5000000000L,
-              "04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG")), // txins
-          0 //lockTime
-          )))
-  def interval: Int = INTERVAL
-  def packetMagic: Long = 0xD9B4BEF9L
-  def port: Int = 8333
-  def proofOfWorkLimit: BigInt = 0 //TODO
-  def targetTimespan: Int = 0 //TODO
-  def dnsSeeds: List[String] = List(
-    "seed.bitcoin.sipa.be",
-    "dnsseed.bluematt.me",
-    "dnsseed.bitcoin.dashjr.org",
-    "bitseed.xf2.org")
-
-}
-* 
-*/
 
 object MainNetParams extends NetworkParameters {
 
@@ -97,6 +53,33 @@ object MainNetParams extends NetworkParameters {
     "dnsseed.bluematt.me",
     "dnsseed.bitcoin.dashjr.org",
     "bitseed.xf2.org")
+
+}
+
+object TestNet3Params extends NetworkParameters {
+
+  def addressHeader: Byte = 0
+  def genesisBlock: Block = {
+    val header: BlockHeader = BlockHeader(
+      1L,
+      Hash(hex"0000000000000000000000000000000000000000000000000000000000000000"),
+      Hash(hex"4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"),
+      1296688602L,
+      0x1d00ffffL,
+      414098458L)
+    Block(
+      header,
+      List()) // TODO: add tx's
+  }
+  def interval: Int = INTERVAL
+  def packetMagic: Long = 0x0B110907
+  def port: Int = 18333
+  def proofOfWorkLimit: BigInt = 0 //TODO
+  def targetTimespan: Int = 0 //TODO
+  def dnsSeeds: List[String] = List(
+    "bitcoin.petertodd.org",
+    "testnet-seed.bitcoin.petertodd.org",
+    "testnet-seed.bluematt.me")
 
 }
 
