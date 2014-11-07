b8b259affef8684e34a51ae7bcf3c7a0c3aad134
yzernik <yzernik@gmail.com>
2014-11-06 23:48:17 -0800

    added test case for initiating handshake in peer connection

--- src/test/scala/com/oohish/bitcoinakkanode/wire/PeerConnectionSpec.scala
+++ src/test/scala/com/oohish/bitcoinakkanode/wire/PeerConnectionSpec.scala
@@ -2,22 +2,22 @@ package com.oohish.bitcoinakkanode.wire
 
 import java.net.InetAddress
 import java.net.InetSocketAddress
-
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
-
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.Matchers
 import org.scalatest.WordSpecLike
-
 import com.oohish.bitcoinakkanode.node.Node
 import com.typesafe.config.ConfigFactory
-
 import akka.actor.ActorSystem
 import akka.actor.actorRef2Scala
 import akka.testkit.DefaultTimeout
 import akka.testkit.ImplicitSender
 import akka.testkit.TestKit
+import akka.testkit.TestProbe
+import com.oohish.bitcoinscodec.messages.Version
+import com.oohish.bitcoinscodec.messages.Ping
+import com.oohish.bitcoinscodec.structures.NetworkAddress
 
 /**
  * a Test to show some TestKit examples
@@ -31,17 +31,23 @@ class PeerConnectionSpec
 
   val remote = new InetSocketAddress(InetAddress.getLocalHost(), 1)
   val local = new InetSocketAddress(InetAddress.getLocalHost(), 2)
-  val peerConnectionRef = system.actorOf(PeerConnection.props(null, null, testActor, remote, local, MainNetParams))
+  val manager = TestProbe()
+  val tcpConnection = TestProbe()
+  val node = TestProbe()
+  val peerConnectionRef = system.actorOf(PeerConnection.props(manager.ref, tcpConnection.ref, node.ref, remote, local, MainNetParams))
 
   override def afterAll {
     shutdown()
   }
 
   "A PeerConnection" should {
-    "Forward a message it receives" in {
+    "initiate a handshake when it receives an Initiate command" in {
       within(500 millis) {
         peerConnectionRef ! PeerConnection.InitiateHandshake()
-        expectMsg(Node.GetVersion(remote, local))
+        node.expectMsg(Node.GetVersion(remote, local))
+        val v = Version(60000, 1, 12345L, NetworkAddress(1, remote), NetworkAddress(1, local), 5555L, "agent", 1, true)
+        node.reply(v)
+        tcpConnection.expectMsg(TCPConnection.OutgoingMessage(v))
       }
     }
   }
