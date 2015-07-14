4195005d14b87290daae9b8437b6c81390de72b4
Jonathan Zernik <yzernik@gmail.com>
2015-07-13 23:24:22 -0700

    fixed shell

--- src/main/scala/io/github/yzernik/reactivebitcoinnode/node/Shell.scala
+++ src/main/scala/io/github/yzernik/reactivebitcoinnode/node/Shell.scala
@@ -1,17 +1,13 @@
 package io.github.yzernik.reactivebitcoinnode.node
 
 import scala.concurrent.Await
-import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
 import com.quantifind.sumac.FieldArgs
 
-import akka.actor.ActorRef
 import akka.actor.ActorSystem
-import akka.pattern.ask
-import akka.util.Timeout.durationToTimeout
-import io.github.yzernik.btcio.actors.BTC.PeerInfo
 
 class NodeArgs extends FieldArgs {
   var network: String = "main"
@@ -24,53 +20,44 @@ class NodeArgs extends FieldArgs {
     }
 }
 
-class CLI(node: ActorRef) {
-
-  /**
-   * Evaluate a single command line command.
-   */
-  def evalCommand(input: String): String = {
-    val cmdpattern = """[\s]*([^\s]+)(.*)""".r
-    input match {
-      case cmdpattern("getpeerinfo", param) =>
-        val r = queryNode(Node.GetPeerInfo).asInstanceOf[List[PeerInfo]]
-        r.toString
-      case cmdpattern("getconnectioncount", param) =>
-        queryNode(Node.GetConnectionCount).asInstanceOf[Int]
-          .toString
-      case cmdpattern("getblockcount", param) =>
-        queryNode(Node.GetBlockCount).asInstanceOf[Int]
-          .toString
-      case _ =>
-        s"command not found: $input"
-    }
-  }
-
-  /**
-   * Ask the Bitcoin node for the result of the query.
-   */
-  private def queryNode(cmd: Node.APICommand) = {
-    val t = (10 seconds)
-    Await.result((node ? cmd)(t), t)
-  }
+object Shell {
 
   /**
    * Repeatedly handle command line commands until the exit.
    */
-  def handleInputs: Unit = {
+  def handleInputs(client: RPCClient): Unit = {
     var ok = true
     do {
       print("reactive-bitcoin-node>")
       val ln = scala.io.StdIn.readLine
       ok = ln != null && ln != "quit" && ln != "exit"
       if (ok && !ln.isEmpty())
-        println(evalCommand(ln))
+        println(evalCommand(client, ln))
     } while (ok)
   }
 
-}
+  /**
+   * Evaluate a single command line command.
+   */
+  def evalCommand(client: RPCClient, input: String): String = {
+    val cmdpattern = """[\s]*([^\s]+)(.*)""".r
+    input match {
+      case cmdpattern("getpeerinfo", param) =>
+        awaitRPC(client.getPeerInfo).toString
+      case cmdpattern("getconnectioncount", param) =>
+        awaitRPC(client.getConnectionCount).toString
+      case cmdpattern("getblockcount", param) =>
+        awaitRPC(client.getBlockCount).toString
+      case _ =>
+        s"command not found: $input"
+    }
+  }
 
-object Shell {
+  /**
+   * Wait for the result of the RPC call.
+   */
+  private def awaitRPC[T](r: Future[T]) =
+    Await.result(r, 10 seconds)
 
   def main(args: Array[String]) {
     val sys = ActorSystem("shellsys")
@@ -80,8 +67,8 @@ object Shell {
       nodeArgs.parse(args)
       println(s"Starting bitcoin node on network: ${nodeArgs.network}")
       val node = sys.actorOf(Node.props(nodeArgs.getNetworkParams), name = "node")
-      val cli = new CLI(node)
-      cli.handleInputs
+      val client = new RPCClient(node)
+      handleInputs(client)
       println(s"Shutting down bitcoin node")
     } catch {
       case e: com.quantifind.sumac.FeedbackException =>
